3d22228b26c5c002f37e03d607cc2e9a4cfe9da2
aniketalhat <aniket.alhat@gmail.com>
2014-12-24 13:24:48 +0900

    if else

---
+++ test.py
@@ -0,0 +1,12 @@
+#start with while
+NUM = 10
+i = 0
+while i <= 10:
+	if i%2 == 0:
+		print i," is even number"
+	else:
+		print i," is odd number"
+	i= i + 1
+	NUM = NUM + 10
+
+print "Value  of" ,NUM
